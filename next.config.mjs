e138026180280a3ca6637088f2d43936fa0eebde
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-09 13:19:27 +0600

    Image Optimization in NextJS

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // https://plus.unsplash.com/premium_photo-1775446842961-c8ba3264b23a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+      // https://i.ibb.co.com/trx2j5Q/rashid-khan.jpg
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/trx2j5Q/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 import React from 'react';
+import shoaib from '../../assets/image/shubu.jpg'
 
 const AboutPage = () => {
   return (
     <div>
       <h2 className='text-6xl font-bold'>This is About Page</h2>
+      <Image width='500' height='300' src='https://i.ibb.co.com/trx2j5Q/rashid-khan.jpg' alt='tamim'></Image>
+     
+      <Image width='500' height='300' src='https://plus.unsplash.com/premium_photo-1775446842961-c8ba3264b23a' alt='mediPic' ></Image>
+      <Image width='500' height='300' src='/shoaib.jpeg' alt='donPic' ></Image>
+      <Image width='500' height='300' src={shoaib} alt='shoaibBhai'></Image>
     </div>
   );
 };
